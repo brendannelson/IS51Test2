5db95fb61359e1e3676904af9c36f4c2f9912635
Brendan Nelson <brendan.nelson49@gmail.com>
2020-05-06 18:53:15 -0700

    finished exam 2

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -14,4 +14,8 @@ export class MainNavComponent implements OnInit {
   ngOnInit() {
   }
 
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'This application was created by Brendan Nelson. (C) 2020');
+  }
+
 }
